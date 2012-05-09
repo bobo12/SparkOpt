02362c472b4453488596ad72e00f26ab8fce2796
Johann Grauzam <johann.grauzam@gmail.com>
2012-05-09 04:36:52 +0200

    kfold last

--- src/main/scala/admm/trials/KFoldTrial.scala
+++ src/main/scala/admm/trials/KFoldTrial.scala
@@ -6,6 +6,7 @@ import admm.stats.SuccessRate._
 import spark.RDD
 import admm.data.ReutersData.ReutersSet
 import admm.opt.SLRSparkImmutable._
+import collection.mutable.HashMap
 
 
 /**
@@ -26,13 +27,32 @@ class KFoldTrial extends SLRLaunchable {
     val sparsityW = kws.get("sparsityW").get.toDouble
     
     // if conf.nSlices > p then :
+    assert(conf.nSlices > p)
 
-    val rddSet = ReutersSetID.rddWithIds(generate_data(sc,conf, sparsityA, sparsityW))
-    val rddSetTrain = sc.parallelize(rddSet.take(p),p).cache()
-    val rddSetValid = sc.parallelize(rddSet.toArray().takeRight(conf.nSlices - p), conf.nSlices - p).cache()
+    val rddSet = ReutersSetID.rddWithIds(generate_data(sc,conf, sparsityA, sparsityW)).cache()
+
+    val kMap = {
+      val pairs1 = rddSet.map(_.id).toArray().toList.take(p)
+      val pairs2 = rddSet.map(_.id).toArray().toList.takeRight(conf.nSlices - p)
+      val pairs = List(pairs1, pairs2).zipWithIndex.map{
+        case (a,b) => (b,a)
+      }
+      HashMap(pairs: _*)
+    }
+    
+    val kSlices = kMap.get(0).get
+    val rddSetTrain = rddSet.filter(slice => kSlices.contains(slice.id))
+    val rddSetValid = rddSet.filter(slice => !kSlices.contains(slice.id))
+
+    //val rddSetTrain = sc.parallelize(rddSet.take(p),p).cache()
+    //val rddSetValid = sc.parallelize(rddSet.toArray().takeRight(conf.nSlices - p), conf.nSlices - p).cache()
     
     //val rddSetTrain = ReutersSetID.rddWithIds(sc.parallelize(generate_data(sc,conf, sparsityA, sparsityW).take(p),p)).cache()
     //val rddSetValid = ReutersSetID.rddWithIds(sc.parallelize(generate_data(sc,conf, sparsityA, sparsityW).toArray().takeRight(conf.nSlices - p), conf.nSlices - p)).cache()
+
+    println("size")
+    println(rddSetTrain.count())
+    println(rddSetValid.count())
 
     val zEstKFold = SolveValidation.kFoldCrossV(rddSetTrain, k, conf)
     val zEstReg = solve(rddSetTrain.asInstanceOf[RDD[ReutersSet]], conf).z
